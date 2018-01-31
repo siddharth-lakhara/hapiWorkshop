5a719cbdf265261584f90ed6fe08442aeded3ff9
lakhara <siddharth.lakhara@gmail.com>
2018-02-01 01:10:05 +0530

    Handler: server giving response

--- handler/handler.test.js
+++ handler/handler.test.js
@@ -2,10 +2,21 @@ const http = require('http');
 
 test('Check server is running', (done)=>{
 	// console.log(soln.info.uri);
-	http.get('http://localhost:8080/siddharth', (response)=>{
+	http.get('http://localhost:8080', (response)=>{
 		response.setEncoding('utf8');
 		// console.log(response.statusCode);
 		expect(response.statusCode).toBe(200);
 		done();
+	});
+});
+
+test('Verify correct request', (done)=>{
+	http.get('http://localhost:8080', (response)=>{
+		response.setEncoding('utf8');
+		response.on('data', (data)=>{
+			// console.log(data);
+			expect(data).toMatch('Im working');
+			done();
+		});
 	});
 });
